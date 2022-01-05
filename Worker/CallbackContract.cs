5247e075f5960d86517fead6d4e96abd2179740d
ognjenp1 <pekecognjen@gmail.com>
2022-01-05 15:36:44 +0100

    doWork logic

--- Worker/CallbackContract.cs
+++ Worker/CallbackContract.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,30 @@ namespace Worker
     {
         public string DoWork(string consumption)
         {
-           return "Radi posao " + consumption +  ".";
+            double greenZoneCost = Double.Parse(ConfigurationManager.AppSettings["greenZoneCost"]);
+            double blueZoneCost = Double.Parse(ConfigurationManager.AppSettings["blueZoneCost"]);
+            double redZoneCost = Double.Parse(ConfigurationManager.AppSettings["redZoneCost"]);
+            float amount = float.Parse(consumption);
+
+            int greenZoneUpperLimit = Int32.Parse(ConfigurationManager.AppSettings["greenZoneUpperLimit"]);
+            int blueZoneUpperLimit = Int32.Parse(ConfigurationManager.AppSettings["blueZoneUpperLimit"]);
+
+            if (amount <= greenZoneUpperLimit)
+            {
+                return (amount * greenZoneCost).ToString();
+            }
+            else if (amount > greenZoneUpperLimit && amount <= blueZoneUpperLimit)
+            {
+                return (greenZoneUpperLimit * greenZoneCost + (amount - greenZoneUpperLimit) * blueZoneCost).ToString();
+            }
+            else if (amount > blueZoneUpperLimit)
+            {
+                return (greenZoneUpperLimit * greenZoneCost + (blueZoneUpperLimit - greenZoneUpperLimit) * blueZoneCost + (amount - blueZoneUpperLimit) * redZoneCost).ToString();
+            }
+            else
+            {
+                return "0";
+            }
         }
     }
 }
